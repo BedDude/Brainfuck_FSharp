825a3c71433456a3c73bcafd192903a7bf5260cc
Ilya Fadeev <fadyaIlya@yandex.ru>
2021-07-20 03:51:05 +0300

    add loop construction

--- src/Program.fs
+++ src/Program.fs
@@ -14,7 +14,7 @@ let readFile path =
     |> Seq.toArray
     
 let createProgram code = 
-    { Code = code; CodePos = 0; Mem = [|for i in 0..30000 -> 0|]; MemPos = 0 }
+    { Code = code; CodePos = 0; Mem = Array.zeroCreate(30000); MemPos = 0 }
 
 let readValue = 
     Console.Read()
@@ -26,25 +26,47 @@ let getNextOp program =
     if program.CodePos < program.Code.Length 
     then Some(program.Code.[program.CodePos])
     else None
-    
-let interpCode program =
-    match program.Code.[program.CodePos] with
-    | '+' -> program.Mem.[program.MemPos] <- program.Mem.[program.MemPos] + 1
-    | '-' -> program.Mem.[program.MemPos] <- program.Mem.[program.MemPos] - 1
-    | '>' -> program.MemPos <- program.MemPos + 1
-    | '<' -> program.MemPos <- program.MemPos - 1
-    | '.' -> printValue program.Mem.[program.MemPos]
-    | ',' -> program.Mem.[program.MemPos] <- readValue
-    program.CodePos <- program.CodePos + 1
+
+let rec startLoop program pos level = 
+    match level with
+    | 0 -> pos
+    | _ -> match program.Code.[pos] with
+           | '[' -> startLoop program (pos - 1) (level - 1)
+           | ']' -> startLoop program (pos - 1) (level + 1)
+           | _ -> startLoop program (pos - 1) level
+
+let rec endLoop program pos level = 
+    match level with
+    | 0 -> pos
+    | _ -> match program.Code.[pos] with
+           | '[' -> endLoop program (pos + 1) (level + 1)
+           | ']' -> endLoop program (pos + 1) (level - 1)
+           | _ -> endLoop program (pos + 1) level
+
+let interpCode prg symb =
+    match symb with
+    | '+' -> prg.Mem.[prg.MemPos] <- prg.Mem.[prg.MemPos] + 1
+    | '-' -> prg.Mem.[prg.MemPos] <- prg.Mem.[prg.MemPos] - 1
+    | '>' -> prg.MemPos <- prg.MemPos + 1
+    | '<' -> prg.MemPos <- prg.MemPos - 1
+    | '.' -> printValue prg.Mem.[prg.MemPos]
+    | ',' -> prg.Mem.[prg.MemPos] <- readValue
+    | '[' -> if prg.Mem.[prg.MemPos] = 0 
+             then prg.CodePos <- endLoop prg (prg.CodePos + 1) 1 
+    | ']' -> if prg.Mem.[prg.MemPos] <> 0
+             then prg.CodePos <- startLoop prg (prg.CodePos - 1) 1
+    | _ -> ()
 
 let rec run program = 
     match getNextOp program with
-    | Some(x) -> interpCode program; run program
+    | Some(x) -> interpCode program x
+                 program.CodePos <- program.CodePos + 1 
+                 run program
     | None -> 0
 
 [<EntryPoint>]
 let main argv =
-    let code = 
+    let code =
         if argv.Length = 0 then Console.ReadLine() |> Seq.toArray
         else readFile argv.[0]
     
